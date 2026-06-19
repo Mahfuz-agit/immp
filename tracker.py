2ecd14a96be52dd0649ed3983f0abe699a60ec53
Mahfuz-agit <rnlnit@protonmail.com>
2026-06-19 09:22:25 +0600

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -88,7 +88,7 @@ Rules:
 
     try:
         response = client.chat.completions.create(
-            model="llama3-70b-8192",
+            model="llama-3.3-70b-versatile",
             messages=[{"role": "user", "content": prompt}],
             max_tokens=2000,
             temperature=0.1
